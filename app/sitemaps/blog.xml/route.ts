5bf3bbbe5baaa5f54234c904b8ba0859cb1fed9c
punniix <chanatip.chid@gmail.com>
2026-08-24 07:49:05 +0700

    seo: include only useful indexable topic hubs in blog sitemap

--- app/sitemaps/blog.xml/route.ts
+++ app/sitemaps/blog.xml/route.ts
@@ -1,15 +1,38 @@
 import { getContentProvider } from "@/lib/content/provider";
+import { BLOG_TOPIC_HUBS, isArticleInSemanticTopic } from "@/lib/content/taxonomy";
 import { renderUrlSet, xmlResponse } from "@/lib/sitemap/xml";
 import { getArticleCanonical, isArticleCanonicalAligned } from "@/lib/content/url";
 
+function latestUpdatedAt(values: string[]) {
+  return values.reduce((latest, value) => (value > latest ? value : latest), values[0] ?? "");
+}
+
 export async function GET() {
   const articles = await getContentProvider().listArticles({ includeDrafts: false });
-  return xmlResponse(
-    renderUrlSet(
-      articles.filter(isArticleCanonicalAligned).map((article) => ({
-        loc: getArticleCanonical(article),
-        lastmod: article.updatedAt,
-      })),
-    ),
-  );
+  const articleEntries = articles.filter(isArticleCanonicalAligned).map((article) => ({
+    loc: getArticleCanonical(article),
+    lastmod: article.updatedAt,
+  }));
+
+  const hubEntries = BLOG_TOPIC_HUBS.flatMap((hub) => {
+    if (!hub.indexable) return [];
+    const relevant = articles.filter((article) =>
+      isArticleInSemanticTopic(
+        {
+          articleSlug: article.slug,
+          categoryTitle: article.category,
+          categorySlug: article.categorySlug,
+          tags: article.tags,
+        },
+        hub.slug,
+      ),
+    );
+    if (!relevant.length) return [];
+    return [{
+      loc: `https://ccpun.com/blog/${hub.slug}/`,
+      lastmod: latestUpdatedAt(relevant.map((article) => article.updatedAt)),
+    }];
+  });
+
+  return xmlResponse(renderUrlSet([...hubEntries, ...articleEntries]));
 }
